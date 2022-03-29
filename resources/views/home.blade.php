df30905d0446573baa26f3709859c924ab78d24b
jukovskii <zhukovskey@gmail.com>
2022-03-29 17:03:38 +0200

    Force https for production env

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel React Boilerplate</title>
+    <title>Expert tile test</title>
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}" defer></script>
